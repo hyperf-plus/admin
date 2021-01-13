5517d03a7cb453df53f8394eba5cca2d7c75378a
Smile <4213509@qq.com>
2021-01-13 23:16:45 +0800

    fix

--- src/Middleware/AuthMiddleware.php
+++ src/Middleware/AuthMiddleware.php
@@ -45,9 +45,9 @@ class AuthMiddleware implements MiddlewareInterface
             //因为导出数据是跳转的浏览器新窗口，所以头信息携带会丢失，这里需要用cookie来判断权限
             Context::override(ServerRequestInterface::class, function (ServerRequestInterface $request) {
                 $token = $request->getCookieParams()[config('cookie_name', 'HPLUSSESSIONID')] ?? null;
-                return $request->withQueryParams([
+                return $request->withQueryParams(array_merge($request->getQueryParams(),[
                     'token' => $token
-                ]);
+                ]));
             });
         }
 
