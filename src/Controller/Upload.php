08ee1daee87eec62ff085c5aaf3be31b604f0ec3
wilbur.yu <ziyanziyu77@gmail.com>
2021-01-23 11:55:45 +0800

    修复上传配置获取不完整的情况

--- src/Controller/Upload.php
+++ src/Controller/Upload.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
  * @contact  4213509@qq.com
  * @license  https://github.com/hyperf-plus/admin/blob/master/LICENSE
  */
-
 namespace HPlus\Admin\Controller;
 
 use HPlus\Admin\Exception\BusinessException;
@@ -17,9 +16,8 @@ use HPlus\Admin\Facades\Admin;
 use HPlus\Route\Annotation\ApiController;
 use HPlus\Route\Annotation\FormData;
 use HPlus\Route\Annotation\PostApi;
+use Hyperf\Contract\ConfigInterface;
 use Hyperf\HttpMessage\Upload\UploadedFile as UploadedFileAlias;
-use Hyperf\HttpServer\Annotation\Controller;
-use Hyperf\HttpServer\Annotation\PostMapping;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Contract\ResponseInterface;
 use Psr\Container\ContainerInterface;
@@ -45,34 +43,40 @@ class Upload
      */
     protected $response;
 
+    /**
+     * @var array
+     */
+    protected $config;
+
     public function __construct(ContainerInterface $container, RequestInterface $request, ResponseInterface $response)
     {
         $this->response = $response;
         $this->request = $request;
         $this->container = $container;
+        $this->config = $this->container->get(ConfigInterface::class)->get('admin.upload') ?? [];
     }
 
     /**
      * @PostApi(path="image")
-     * @FormData(key="file",type="file",rule="file")
-     * @FormData(key="path",default="avatar")
+     * @FormData(key="file", type="file", rule="file")
+     * @FormData(key="path", default="avatar")
      */
     public function image()
     {
         $file = $this->request->file('file');
-        if (empty($file) || !$file->isValid()) {
+        if (empty($file) || ! $file->isValid()) {
             throw new BusinessException(400, '请选择正确的文件！');
         }
-        $uploadConfig = config('admin.upload');
-        if ($file->getSize() > ($uploadConfig['image_size'] ?? 1024 * 1024 * 5)) {
+        $fileSize = $this->config['file_size'] ?? 1024 * 1024 * 50;
+        if ($file->getSize() > $fileSize) {
             throw new BusinessException(1000, '文件不能大于！' . ($fileSize / 1024 / 1024) . 'MB');
         }
-        $imageMimes = explode(',', $uploadConfig['image_mimes'] ?? 'jpeg,bmp,png,gif,jpg');
-        if (!in_array(strtolower($file->getExtension()), $imageMimes)) {
+        $imageMimes = explode(',', $this->config['image_mimes'] ?? 'jpeg,bmp,png,gif,jpg');
+        if (! in_array(strtolower($file->getExtension()), $imageMimes)) {
             throw new BusinessException(1000, '后缀不允许！');
         }
         #检测类型
-        if (!in_array(strtolower($file->getClientMediaType()), $uploadConfig['media_types'] ?? ['image/gif', 'image/jpeg', 'image/jpg', 'image/png', 'image/pjpeg', 'image/x-png'])) {
+        if (! in_array(strtolower($file->getClientMediaType()), $this->config['media_types'] ?? ['image/gif', 'image/jpeg', 'image/jpg', 'image/png', 'image/pjpeg', 'image/x-png'])) {
             throw new BusinessException(1000, '不允许上传此文件！');
         }
         return Admin::response($this->saveFiles($file, 'image'));
@@ -80,22 +84,22 @@ class Upload
 
     /**
      * @PostApi(path="file")
-     * @FormData(key="file",type="file",rule="file")
-     * @FormData(key="path",default="file")
+     * @FormData(key="file", type="file", rule="file")
+     * @FormData(key="path", default="file")
      */
     public function file()
     {
         $file = $this->request->file('file');
-        if (empty($file) || !$file->isValid()) {
+        if (empty($file) || ! $file->isValid()) {
             throw new BusinessException(400, '请选择正确的文件！');
         }
-        $fileSize = config('admin.upload.file_size', 1024 * 1024 * 50);
+        $fileSize = $this->config['file_size'] ?? 1024 * 1024 * 50;
         if ($file->getSize() > $fileSize) {
             throw new BusinessException(1000, '文件不能大于！' . ($fileSize / 1024 / 1024) . 'MB');
         }
         #检测类型
-        $imageMimes = explode(',', $config['file_mimes'] ?? 'txt,sql,zip,rar,ppt,word,xls,xlsx,doc,docx');
-        if (!in_array(strtolower($file->getExtension()), $imageMimes)) {
+        $imageMimes = explode(',', $this->config['file_mimes'] ?? 'txt,sql,zip,rar,ppt,word,xls,xlsx,doc,docx');
+        if (! in_array(strtolower($file->getExtension()), $imageMimes)) {
             throw new BusinessException(1000, '类型不允许！');
         }
         return Admin::response($this->saveFiles($file, 'files'));
@@ -103,7 +107,7 @@ class Upload
 
     protected function saveFiles(UploadedFileAlias $file, $fileName = 'image')
     {
-        $file_name = config('admin.upload.save_path', '/upload');
+        $file_name = $this->config['save_path'] ?? '/upload';
         $file_name = $file_name . '/' . $fileName . '/' . date('Ym') . '/' . date('d') . '/' . uuid(16) . '.' . strtolower($file->getExtension());
         Storage()->getDriver()->put($file_name, $file->getStream()->getContents());
         return [
